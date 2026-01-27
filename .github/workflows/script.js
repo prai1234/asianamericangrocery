0b81381481bbaa1eeb3d477595d72c73cb718458
Prahlad B Rai <catmando849@gmail.com>
2026-01-26 19:36:25 -0500

    Delete .github/workflows/script.js
    Signed-off-by: Prahlad B Rai <catmando849@gmail.com>

--- .github/workflows/script.js
+++
@@ -1,9 +0,0 @@
-function showPage(pageId) {
-    const pages = document.querySelectorAll('.page');
-
-    pages.forEach(page => {
-        page.classList.remove('active');
-    });
-
-    document.getElementById(pageId).classList.add('active');
-}
